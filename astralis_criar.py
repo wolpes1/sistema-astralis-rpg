0c1afd8259697014e657216989e1bddb86e95cf6
Renan Lopes Cavalcante <renanlc1857@gmail.com>
2023-04-10 23:04:31 -0300

    Alterações maiores
    Criação de um arquivo para o módulo de criação de personagens, adição de códigos estéticos.

---
+++ astralis_criar.py
@@ -0,0 +1,39 @@
+import os
+import astralis_personagem as personagem
+
+def limpar_tela():
+    os.system('cls')
+
+def criar_personagem():
+    nome_personagem = input('Qual será o nome do personagem?\n')
+    limpar_tela()
+    jogador_personagem = input('E quem será o jogador controlando o personagem?\n')
+    limpar_tela()
+    for_personagem = int(input('E seus niveis de:\nForça:'))
+    limpar_tela()
+    hab_personagem = int(input('E seus niveis de:\nHabilidade:'))
+    limpar_tela()
+    res_personagem = int(input('E seus niveis de:\nResistência:'))
+    limpar_tela()
+    vit_personagem = int(input('E seus niveis de:\nVitalidade:'))
+    limpar_tela()
+    int_personagem = int(input('E seus niveis de:\nInteligência:'))
+    limpar_tela()
+    afi_personagem = int(input('E seus niveis de:\nAfinidade Mágica:'))
+    limpar_tela()
+    print('Definindo personagem...')
+    ficha_personagem = personagem.Personagem_jogador
+    ficha_personagem.definir_nome(ficha_personagem, nome_personagem)
+    ficha_personagem.definir_jogador(ficha_personagem, jogador_personagem)
+    ficha_personagem.definir_for(ficha_personagem, for_personagem)
+    ficha_personagem.definir_hab(ficha_personagem, hab_personagem)
+    ficha_personagem.definir_int(ficha_personagem, int_personagem)
+    ficha_personagem.definir_res(ficha_personagem, res_personagem)
+    ficha_personagem.definir_vit(ficha_personagem, vit_personagem)
+    ficha_personagem.definir_afi(ficha_personagem, afi_personagem)
+    input('Pressione enter para continuar...')
+    limpar_tela()
+    print('Personagem definido!')
+    print(ficha_personagem.__str__(ficha_personagem))
+    input('Pressione enter para finalizar.')
+    return ficha_personagem
--- astralis_menu_principal.py
+++ astralis_menu_principal.py
@@ -1,7 +1,7 @@
 import astralis_carregar as carregamento
 import astralis_salvar as salvamento
-import astralis_personagem as personagem
 import astralis_rolagem as rolagem_dados
+import astralis_criar as criar
 import os
 
 def limpar_tela():
@@ -29,51 +29,28 @@ while opcao_escolhida != 'sair':
     Opção:""").lower()
 
     match opcao_escolhida:
+        
         case 'criar':
             limpar_tela()
-            nome_personagem = input('Qual será o nome do personagem?')
+            ficha_personagem = criar.criar_personagem()
             limpar_tela()
-            jogador_personagem = input('E quem será o jogador controlando o personagem?')
-            limpar_tela()
-            for_personagem = int(input('E seus niveis de:\nForça:'))
-            limpar_tela()
-            hab_personagem = int(input('E seus niveis de:\nHabilidade:'))
-            limpar_tela()
-            res_personagem = int(input('E seus niveis de:\nResistência:'))
-            limpar_tela()
-            vit_personagem = int(input('E seus niveis de:\nVitalidade:'))
-            limpar_tela()
-            int_personagem = int(input('E seus niveis de:\nInteligência:'))
-            limpar_tela()
-            afi_personagem = int(input('E seus niveis de:\nAfinidade Mágica:'))
-            limpar_tela()
-            print('Definindo personagem...')
-            ficha_personagem = personagem.Personagem_jogador
-            ficha_personagem.definir_nome(ficha_personagem, nome_personagem)
-            ficha_personagem.definir_jogador(ficha_personagem, jogador_personagem)
-            ficha_personagem.definir_for(ficha_personagem, for_personagem)
-            ficha_personagem.definir_hab(ficha_personagem, hab_personagem)
-            ficha_personagem.definir_int(ficha_personagem, int_personagem)
-            ficha_personagem.definir_res(ficha_personagem, res_personagem)
-            ficha_personagem.definir_vit(ficha_personagem, vit_personagem)
-            ficha_personagem.definir_afi(ficha_personagem, afi_personagem)
-            input('Pressione enter para continuar...')
-            limpar_tela()
-            print('Personagem definido!')
-            print(ficha_personagem.__str__(ficha_personagem))
-            input('Pressione enter para finalizar.')
-
         case 'salvar':
-            if ficha_personagem == None:
-                input('Não existe ficha! Crie uma para poder salvar.\nPressione enter para continuar.')
-                limpar_tela()
-            else:
+
+            limpar_tela()  
+
+            try:
+  
                 salvamento.salvar_personagem(ficha_personagem, ficha_personagem.nome)
                 input('Salvamento concluido! Pressione Enter para continuar.')
                 limpar_tela()
+            except:
+                input('Não há personagem para salvar!\nPressione enter para continuar.')
 
         case 'carregar':
+            
+            limpar_tela()
             nome_salvo = input('Qual o nome do seu personagem?').capitalize()
+            limpar_tela()
 
             try:
                 ficha_carregada = carregamento.carregar_dados(nome_salvo)
@@ -85,19 +62,27 @@ while opcao_escolhida != 'sair':
                 limpar_tela()
 
         case 'visualizar':
-            print(ficha_personagem.__str__(ficha_personagem))
-            input('Pressione Enter para continuar.')
+            limpar_tela()
+            try:
+                print(ficha_personagem.__str__(ficha_personagem))
+                input('Pressione Enter para continuar.')
+                limpar_tela()
+            except:
+                input('Não há personagem carregado!\nPressione enter para continuar.')
 
 
         case 'rolar':
+            limpar_tela()
             rolagem_dados.execucao()
             input('Pressione Enter para continuar.')
             limpar_tela()
 
         case 'sair':
+            limpar_tela()
             input('Obrigade por usar este app!\nPressione enter para sair.')
             limpar_tela()
             break
 
         case _:
+            limpar_tela()
             input('Esta opção não é válida!\nPressione enter para continuar.')
